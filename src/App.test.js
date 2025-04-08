90cb3d80b3534ac857e5cb2a30f74f3c7cc04728
Gary662 <garysimwawa25@gmail.com>
2025-04-08 13:51:44 -0600

    updated App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('shows name on page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameElement = screen.getByText(/Gary/i);
+  expect(nameElement).toBeInTheDocument();
 });
